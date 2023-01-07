f3e1951c2e6210504fa4d633f81996c6ba51fcd5
whume@costar.com <willhume05@gmail.com>
2023-01-07 14:49:04 -0500

    add product sum

---
+++ Easy/product-sum.py
@@ -0,0 +1,10 @@
+# Tip: You can use the type(element) function to check whether an item
+# is a list or an integer.
+def productSum(array, multiplier = 1):
+    sum = 0
+    for element in array:
+        if type(element) is list:
+            sum += productSum(element, multiplier + 1)
+        else:
+            sum += element
+    return sum * multiplier
