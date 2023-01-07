4ce46d5cd5e0ed0688b44b0ebe42078edd604dbb
whume@costar.com <willhume05@gmail.com>
2023-01-07 14:26:51 -0500

    reverse linked list  added

---
+++ Medium/reverse-linked-list.py
@@ -0,0 +1,16 @@
+# This is an input class. Do not edit.
+class LinkedList:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+def reverseLinkedList(head):
+    p1, p2 = None, head
+      
+    while p2 is not None:
+        p3 = p2.next
+        p2.next = p1
+        p1 = p2
+        p2 = p3
+    return p1
